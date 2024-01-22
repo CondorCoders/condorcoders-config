7366ce810ef4cd4ace9f82544d5a32147f30f14e
Condor Coders <123217980+CondorCoders@users.noreply.github.com>
2024-01-22 22:59:50 +1000

    Create events page config

---
+++ pages/events/2024/voces-femeninas-en-steam-2024.js
@@ -0,0 +1,217 @@
+const event = {
+  banner: {
+    image:
+      "https://images.pexels.com/photos/8540798/pexels-photo-8540798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Voces Femeninas en STEAM 2024",
+    time: 1707544800000,
+    description:
+      "Celebra el Día de la Mujer y la Niña en Ciencia (11F) sumándote a este evento para explorar y aprender de destacadas mujeres en STEAM.",
+    calendarLink:
+      "https://www.google.com/calendar/render?action=TEMPLATE&text=Voces%20Femeninas%20en%20STEAM%20(D%C3%ADa%20Internacional%20de%20la%20Ni%C3%B1a%20y%20Mujer%20en%20la%20Ciencia)&dates=20240211T210000Z/20240211T230000Z&details=EVENTO%20ONLINE%20en%20https%3A%2F%2Fwww.youtube.com%2F%40condorcoders%0A%0AParticipa%20en%20una%20inspiradora%20jornada%20en%20conmemoraci%C3%B3n%20del%20D%C3%ADa%20de%20la%20Mujer%20y%20la%20Ni%C3%B1a%20en%20Ciencia%20(11F).%20Descubre%20historias%20fascinantes%20y%20aprende%20de%20destacadas%20mujeres%20en%20la%20ciencia%2C%20participa%20en%20sorteos%20y%20ten%20la%20oportunidad%20de%20ganar%20incre%C3%ADbles%20premios.%0A&location=https%3A%2F%2Fwww.youtube.com%2F%40condorcoders&sf=true&output=xml",
+    eventLink: "https://www.youtube.com/@condorcoders",
+  },
+  speakers: {
+    title: "Invitadas",
+    description:
+      "Conoce a nuestras destacadas invitadas que liderarán la conversación en nuestro evento del Día de la Mujer y la Niña en Ciencia (11F).",
+    profiles: [
+      {
+        name: "Sofía Juarez Moran",
+        title:
+          "Parte de la Primera Tripulación de niñas Ecuatorianas que fueron a la Nasa del proyecto 'Ella es Astronauta'",
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        socials: [
+          {
+            icon: "instagram",
+            href: "https://www.instagram.com/yosofiajuarez/",
+          },
+        ],
+      },
+      {
+        name: "Mazzarello Gaming",
+        title:
+          "Estudiantes de la Unidad Educativa Fiscomisional Salesiana Santa María Mazzarello",
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        socials: [
+          {
+            icon: "instagram",
+            href: "https://www.instagram.com/mazzarellogaming/",
+          },
+        ],
+      },
+      {
+        name: "Sara Gallegos",
+        title:
+          "Licenciada en Física y Divulgadora de contenido de científico como Morra Científica",
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        socials: [
+          {
+            icon: "instagram",
+            href: "https://www.instagram.com/morra_cientifica/",
+          },
+          {
+            icon: "tiktok",
+            href: "https://www.tiktok.com/@morracientifica",
+          },
+        ],
+      },
+      {
+        name: "Jazmín Guayco",
+        title: "Licenciada en Biotecnología Vegetal y Cultivos",
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        socials: [
+          {
+            icon: "linkedin",
+            href: "https://au.linkedin.com/in/jazmin-guayco-sigcha-5595b8265",
+          },
+        ],
+      },
+      {
+        name: "Cristina Ocaña Gallegos",
+        title: "Licenciada en Biotecnología Vegetal y Cultivos",
+        image:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        socials: [
+          {
+            icon: "instagram",
+            href: "https://www.instagram.com/crissy_96/",
+          },
+          {
+            icon: "linkedin",
+            href: "https://www.linkedin.com/in/crisdom96",
+          },
+        ],
+      },
+    ],
+  },
+  sponsors: {
+    title: "Sponsors",
+    description: "Agradecemos a nuestros valiosos patrocinadores por su apoyo.",
+    brands: {
+      juniors: [
+        {
+          logo: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          name: "Centro de Estudios de Cine y Televisión",
+          link: "",
+        },
+      ],
+      mid: [
+        {
+          logo: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          name: "Centro de Estudios de Cine y Televisión",
+          href: "",
+        },
+      ],
+      senior: [
+        {
+          logo: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          name: "Centro de Estudios de Cine y Televisión",
+          href: "",
+        },
+      ],
+    },
+  },
+  agenda: {
+    title: "Agenda",
+    description:
+      "Desde charlas hasta sorteos, no te pierdas de esta jornada ofrece una experiencia enriquecedora para todos los participantes.",
+
+    itinerary: [
+      {
+        start: 1707544800000,
+        end: 1707545400000,
+        heading: "Bienvenida",
+        description:
+          "Hablamos de la jornada de hoy y cómo participar en los sorteos.",
+        speaker: {
+          name: "Sofía Grijalva",
+          title: "Programadora Frontend y Creadora de Contenido ",
+          image:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      },
+      {
+        start: 1707545400000,
+        end: 1707546600000,
+        heading: "Mi experiencia en la NASA",
+        description:
+          "Conoce la experiencia de ir a la NASA como parte de la Primera Tripulación de niñas ecuatorianas que tuvieron la oportunidad de visitar la NASA, gracias al proyecto 'Ella es Astronauta' de la Fundación 'She Is'.",
+        speaker: {
+          name: "Sofía Juarez Moran",
+          image:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      },
+      {
+        start: 1707546600000,
+        end: 1707547800000,
+        heading: "Mazzarello Gaming",
+        description:
+          "Estudiantes de la Unidad Educativa Fiscomisional Salesiana Santa María Mazzarello desarrollaron el proyecto denominado 'Mazzarello Gaming' con la finalidad de aportar a la sociedad con esta idea innovadora a través de los video juegos.",
+        speaker: {
+          name: "Mazzarello Gaming",
+          image:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      },
+      {
+        start: 1707547800000,
+        end: 1707548400000,
+        heading: "Sorteo",
+        description: "1x curso de Udemy.",
+      },
+      {
+        start: 1707548400000,
+        end: 1707549600000,
+        heading: "Mi experiencia estudiando Física",
+        description:
+          "Sara compartirá cómo logró compaginar su dedicación al estudio de la Física con la emocionante labor de crear y divulgar contenido científico en las redes sociales.",
+        speaker: {
+          name: "Sara Gallegos",
+          title:
+            "Licenciada en Física y Divulgadora de contenido de científico como Morra Científica",
+          image:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      },
+      {
+        start: 1707549600000,
+        end: 1707550800000,
+        heading: "Mi trayectoria hacia la investigación clínica",
+        description:
+          "Desde sus primeros días de formación hasta su actual búsqueda de un doctorado, Jazmín Guayco compartirá sus experiencias, desafíos superados y lecciones aprendidas en el campo de la investigación médica.",
+        speaker: {
+          name: "Jazmín Guayco",
+          title:
+            "Candidata a PhD en el Centro de Investigación Torácica de la Universidad the Queensland (UQTRC)",
+          image:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      },
+      {
+        start: 1707550800000,
+        end: 1707552000000,
+        heading:
+          "Experiencias personales en la carrera de biotecnología vegetal",
+        description:
+          "En esta charla, Cristina compartirá las vivencias que la llevaron a sumergirse en campo de la biotecnología vegetal. Descubrirás los desafíos superados, los logros alcanzados y las lecciones valiosas aprendidas a lo largo de su trayectoria",
+        speaker: {
+          name: "Cristina Ocaña Gallegos",
+          title: "Licenciada en Biotecnología Vegetal y Cultivos.",
+          image:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      },
+      {
+        start: 1707552000000,
+        end: 1707552600000,
+        heading: "Sorteo",
+        description: "1x curso de Udemy.",
+      },
+    ],
+  },
+};
